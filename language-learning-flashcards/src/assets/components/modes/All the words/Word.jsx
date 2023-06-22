5c96300527eeaa3f33973278a011ed9681d82070
Inna <melikhovainna@gmail.com>
2023-06-22 17:03:18 +0300

    states adding

--- language-learning-flashcards/src/assets/components/modes/All the words/Word.jsx
+++ language-learning-flashcards/src/assets/components/modes/All the words/Word.jsx
@@ -11,27 +11,27 @@ export default function Word( props ) {
 
   function changeButtonsState(buttonsState){
     console.log(buttonsState);
-    setButtonState(buttonsState? false: true);
+    setButtonState(!buttonsState);
     // console.log(buttonsState);
   }
 
   return(
     buttonsState?
-      <section className="Word">
+      <section className="Word" >
         <div className="Word__property">{ props.word }</div>
         <div  className="Word__property">{ props.transcription }</div>
         <div  className="Word__property">{ props.translation }</div>
         <div className="Word__options">
-          <ButtonEdit onClick = { changeButtonsState }></ButtonEdit>
+          <ButtonEdit onClick={changeButtonsState}></ButtonEdit>
           <ButtonDelete></ButtonDelete>
         </div>
       </section>
-      : <section className="Word Word__active">
+      : <section className="Word Word__active" >
         <div className="Word__property"><input type="text" value={ props.word } /></div>
         <div className="Word__property"><input type="text" value={ props.transcription }/></div>
         <div className="Word__property"><input type="text" value={ props.translation }/></div>
         <div className="Word__options">
-          <ButtonSave onClick = { changeButtonsState }></ButtonSave>
+          <ButtonSave onClick={changeButtonsState}></ButtonSave>
           <ButtonDelete></ButtonDelete>
         </div>
       </section>

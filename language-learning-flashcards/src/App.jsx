a22845962d72a290f7c54d768e0faf42ee0d8712
Inna <melikhovainna@gmail.com>
2023-06-29 15:20:31 +0300

    CSSTransition added

--- language-learning-flashcards/src/App.jsx
+++ language-learning-flashcards/src/App.jsx
@@ -18,6 +18,7 @@ function App() {
       {/* <AllTheWords /> */}
 
       <TrainingMode /> 
+
     </div>
   );
 }
--- language-learning-flashcards/src/assets/components/modes/TrainingMode/WordCard.jsx
+++ language-learning-flashcards/src/assets/components/modes/TrainingMode/WordCard.jsx
@@ -1,5 +1,5 @@
-import React, {useState} from 'react';
-// import { CSSTransition } from 'react-transition-group';
+import React, { useState } from 'react';
+import { CSSTransition } from 'react-transition-group';
 
 import page from '../../../images/page.png';
 import './WordCard.scss';
@@ -14,11 +14,12 @@ export default function WordCard( props ){
     <div className = "WordCard" >
       <img src = { page } alt = "Карточка слова в виде тетрадного листа" />
       <div className = "word">{ props.word }</div>
-        {translateCheck?
-          <div className = { `word-translate ${ translateCheck }`}>
-            { props.translation }</div>
-          : <div className = "button-check" onClick = { handleChange }>
-            Проверить</div>  }
+      <CSSTransition in = { translateCheck } timeout = { 1000 } classNames = "translate-transition">
+        {translateCheck? 
+          <div className =  "word-translate">{ props.translation } </div>
+          : 
+          <div className = "button-check" onClick = { handleChange }>Проверить</div> }
+      </CSSTransition>
     </div>
   );
 }
